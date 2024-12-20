847e65a26ea87d97655906c38195755a0aff4637
iennhu <140428284+iennhu@users.noreply.github.com>
2024-12-20 22:53:00 +0700

    staff  3

--- ban_ve_routes.py
+++ ban_ve_routes.py
@@ -82,7 +82,147 @@ def danh_sach_ban_ve():
         flight_list=flight_list,
         role=role
     )
+    
+@ban_ve_routes.route('/ban_ve/<id>/<schedule_flight_id>', methods=['GET', 'POST'])
+def ban_ve(id, schedule_flight_id):
+    role = session.get('user', {}).get('role', 'Customer')
 
+    if request.method == 'GET':
+        # Lấy thông tin vé và các liên kết khác
+        ticket = db.session.query(Ticket)\
+            .join(Flight, Ticket.flight_id == Flight.id)\
+            .join(FlightSchedule, Flight.id == FlightSchedule.flight_id)\
+            .filter(Ticket.id == id).first()
+
+        schedule_flight = FlightSchedule.query.filter_by(id=schedule_flight_id).first()
+
+        rule = db.session.query(Rule).first()
+
+        if not ticket or not rule:
+            flash('Không tìm thấy thông tin chuyến bay hoặc quy định.')
+            return redirect(url_for('ban_ve_routes.danh_sach_ban_ve'))
+
+        # Kiểm tra thời gian đặt vé
+        time_sell = schedule_flight.start_date - timedelta(hours=rule.time_sell)
+        if datetime.now() >= time_sell:
+            flash('Không thể bán vé cho chuyến bay này do đã quá thời gian bán vé.')
+            return redirect(url_for('ban_ve_routes.danh_sach_ban_ve'))
+
+         # Tính số ghế đã đặt cho Hạng 1
+        booked_seats_class_1 = db.session.query(func.sum(BookingTicket.quantity))\
+                    .join(Ticket, BookingTicket.ticket_id == Ticket.id)\
+                    .join(Booking, BookingTicket.booking_id == Booking.id)\
+                    .filter(Booking.flight_schedule_id == schedule_flight_id, Ticket.ticket_class == 'Hạng 1')\
+                    .scalar() or 0  # Trả về 0 nếu không có vé nào được đặt
+
+        booked_seats_class_2 = db.session.query(func.sum(BookingTicket.quantity))\
+                    .join(Ticket, BookingTicket.ticket_id == Ticket.id)\
+                    .join(Booking, BookingTicket.booking_id == Booking.id)\
+                    .filter(Booking.flight_schedule_id == schedule_flight_id, Ticket.ticket_class == 'Hạng 2')\
+                    .scalar() or 0
+
+        # Kiểm tra ghế còn trống
+        quantity_class_1 = schedule_flight.quantity_class1
+        quantity_class_2 = schedule_flight.quantity_class2
+
+        if booked_seats_class_1 < quantity_class_1 or booked_seats_class_2 < quantity_class_2:
+            return render_template('ban_ve.html', ticket=ticket, schedule_flight_id=schedule_flight_id, role=role)
+
+        flash('Không thể đặt vé cho chuyến bay này do hết chỗ.')
+        return redirect(url_for('ban_ve_routes.danh_sach_ban_ve'))
+
+    elif request.method == 'POST':
+        # Lấy thông tin từ form
+        customer_id = session.get('user', {}).get('id')  # Lấy id người dùng từ session
+        if not customer_id:
+            flash('Bạn cần đăng nhập để thực hiện thanh toán.', 'error')
+            return redirect(url_for('auth.login'))
+        fullname = request.form['fullname']
+        identity_card = request.form['identity_card']
+        phone_number = request.form['phone_number']
+        amount = float(request.form.get('price', 0))
+        id_post = request.form['id']
+        quantity = int(request.form.get('quantity', 1))
+
+        # Kiểm tra thông tin vé
+        ticket = db.session.query(Ticket)\
+            .join(Flight, Ticket.flight_id == Flight.id)\
+            .join(FlightSchedule, Flight.id == FlightSchedule.flight_id)\
+            .filter(Ticket.id == id).first()
+            
+        schedule_flight = FlightSchedule.query.filter_by(id=schedule_flight_id).first()
+
+        rule = db.session.query(Rule).first()
+
+        if not ticket or not rule:
+            flash('Không tìm thấy thông tin chuyến bay hoặc quy định.')
+            return redirect(url_for('ban_ve_routes.danh_sach_ban_ve'))
+
+
+        # Kiểm tra thời gian bán vé
+        time_sell = schedule_flight.start_date - timedelta(hours=rule.time_sell)
+        if datetime.now() >= time_sell:
+            flash('Không thể bán vé cho chuyến bay này do đã quá thời gian bán vé.')
+            return redirect(url_for('ban_ve_routes.danh_sach_ban_ve'))
+
+        # Tính số ghế đã đặt cho Hạng 1
+        booked_seats_class_1 = db.session.query(func.sum(BookingTicket.quantity))\
+                    .join(Ticket, BookingTicket.ticket_id == Ticket.id)\
+                    .join(Booking, BookingTicket.booking_id == Booking.id)\
+                    .filter(Booking.flight_schedule_id == schedule_flight_id, Ticket.ticket_class == 'Hạng 1')\
+                    .scalar() or 0  # Trả về 0 nếu không có vé nào được đặt
+
+        booked_seats_class_2 = db.session.query(func.sum(BookingTicket.quantity))\
+                    .join(Ticket, BookingTicket.ticket_id == Ticket.id)\
+                    .join(Booking, BookingTicket.booking_id == Booking.id)\
+                    .filter(Booking.flight_schedule_id == schedule_flight_id, Ticket.ticket_class == 'Hạng 2')\
+                    .scalar() or 0
+
+        # Lấy tổng số ghế của từng hạng từ thông tin chuyến bay
+        quantity_class_1 = schedule_flight.quantity_class1
+        quantity_class_2 = schedule_flight.quantity_class2
+
+        # Kiểm tra ghế trống
+        if booked_seats_class_1 >= quantity_class_1 and booked_seats_class_2 >= quantity_class_2:
+            flash('Không thể đặt vé cho chuyến bay này do hết chỗ.')
+            return redirect(url_for('ban_ve_routes.danh_sach_ban_ve'))
+        
+        # Tạo thông tin đặt vé
+        booking = Booking(
+            fullname=fullname,  # Sửa "fullname" thành "fullname" theo tên cột trong model
+            identity_card=identity_card,  # Sửa "identity_card" thành "identity_card"
+            phone_number=phone_number,  # Sửa "phone_number" thành "phone_number"
+            customer_id=customer_id,  # Thay bằng thông tin khách hàng liên quan
+            flight_schedule_id=schedule_flight_id  # Sửa "schedule_flight_id" thành "flight_schedule_id"
+        )
+        
+        db.session.add(booking)
+        db.session.commit()
+
+        # Lấy mã đặt vé vừa được thêm
+        booking_id = booking.id
+
+        booking_ticket = BookingTicket(
+            booking_id=booking_id,
+            ticket_id=id_post,
+            quantity=quantity
+        )
+        db.session.add(booking_ticket)
+        db.session.commit()
+
+        # Thêm thông tin thanh toán
+        
+        thanh_toan = Payment(
+            booking_id=booking_id, 
+            amount=amount,  
+            payment_method='Tại quầy',  
+            payment_date=datetime.utcnow(), 
+        )
+        db.session.add(thanh_toan)
+        db.session.commit()
+
+        flash('Bán vé và thanh toán thành công.')
+        return redirect(url_for('ban_ve_routes.thong_tin_ve', booking_id=booking_id))
 
 
 @ban_ve_routes.route('/thong_tin_ve/<booking_id>', methods=['GET'])
